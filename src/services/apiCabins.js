6596f3b56ec379776048d0b1c67d5329626335e5
Rahimah Ansari <rahimeansari7@gmail.com>
2024-07-21 20:52:58 +0430

    Fetching Cobin Data

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -4,8 +4,8 @@ export async function getCabins() {
   const { data, error } = await supabase.from('cabins').select('*');
 
   if (error) {
-    console.log(error);
-    throw new Error('Cabins could not bet loaded');
+    console.error(error);
+    throw new Error('Cabins could not be loaded');
   }
 
   return data;
